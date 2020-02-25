a6dfd181b04b8043533483232d8881e4211f86b1
renatoreis <renato.reis@mjv.com.br>
2020-02-25 20:00:11 -0300

    Updates

---
+++ precache-manifest.315d160f9537b45efc7110ac2915359f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d02b6913ab4e296e566df5aa92558f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb5bbccb173705290439",
+    "url": "/monsters-rolodex/static/css/main.c716bad7.chunk.css"
+  },
+  {
+    "revision": "f18ec448c3ab67b3b179",
+    "url": "/monsters-rolodex/static/js/2.9e4b70aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.9e4b70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5bbccb173705290439",
+    "url": "/monsters-rolodex/static/js/main.9438dcf8.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
